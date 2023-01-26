89dc30af3abbe3a6e6e83f1f0cf1e0cb0ea59e71
gidesa <gidesa@interfree.it>
2023-01-26 18:26:01 +0100

    Add files via upload

---
+++ examples-Delphi/datamatrixTest/frmDatamatrixTest.pas
@@ -0,0 +1,107 @@
+{ Test for Opencv  Datamatrix capture class  with ocvWrapper using webcam
+
+  Copyright (C) 2023 Giandomenico De Sanctis gidesay@yahoo.com
+
+  This source is free software; you can redistribute it and/or modify it under
+  the terms of the GNU General Public License as published by the Free
+  Software Foundation; either version 2 of the License, or (at your option)
+  any later version.
+
+  This code is distributed in the hope that it will be useful, but WITHOUT ANY
+  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+  details.
+
+  A copy of the GNU General Public License is available on the World Wide Web
+  at <http://www.gnu.org/copyleft/gpl.html>. You can also obtain it by writing
+  to the Free Software Foundation, Inc., 51 Franklin Street - Fifth Floor,
+  Boston, MA 02110-1335, USA.
+}
+unit frmDatamatrixTest;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs,
+  OPENCVWrapper,
+  Vcl.StdCtrls;
+
+
+type
+  TForm6 = class(TForm)
+    btnStart: TButton;
+    lbl1: TLabel;
+    procedure btnStartClick(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form6: TForm6;
+
+implementation
+
+{$R *.dfm}
+const
+  window_name = 'Datamatrix detection';
+
+
+
+procedure TForm6.btnStartClick(Sender: TObject);
+var
+    i: Integer;
+    winame: CvString_t;
+    capture: PCvVideoCapture_t;
+    c: Integer;
+    frame: PCvMat_t;
+    framegray: PCvMat_t;
+    corners: PCvMat_t;
+    codes: PCvvector_string;
+    vecempty: PCvvector_Mat;
+begin
+try
+  btnStart.Enabled:=False;
+  winame.pstr:= PAnsiChar(AnsiString(window_name));
+
+  capture:=pCvVideoCaptureCreate();
+  frame:=pCvMatCreateEmpty();
+  framegray:=pCvMatCreateEmpty();
+  corners:=pCvMatCreateEmpty();
+  codes:=pCvVectorstringCreate(0);
+  vecempty:=pCvVectorMatCreate(0);
+
+  pCvVideoCaptureopen2(capture, 0);
+  if not(pCvVideoCaptureisOpened(capture)) then Exit;
+  while True do
+  begin
+     if not(pCvVideoCaptureread(capture, frame)) then Break;
+
+     pCvcvtColor(frame, framegray, COLOR_BGR2GRAY, 0);
+
+     pCvequalizeHist(framegray, framegray);
+
+     pCvfindDataMatrix(framegray, codes, corners, vecempty);
+     pCvdrawDataMatrixCodes(frame, codes, corners);
+
+
+     pCvimshow(@winame, frame );
+     c:=pCvwaitKey(10);
+     if Char(c) = 'c' then Break;
+  end;
+  pCvdestroyWindow(@winame);
+  btnStart.Enabled:=True;
+finally
+  pCvVideoCaptureDelete(capture);
+  pCvMatDelete(frame);
+  pCvMatDelete(framegray);
+  pCvMatDelete(corners);
+  pCvVectorstringDelete(codes);
+  pCvVectorMatDelete(vecempty);
+end;
+
+end;
+
+end.
