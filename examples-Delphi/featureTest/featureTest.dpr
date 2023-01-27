07711954c6a355bc98f76e5fd8033311f8084b16
gidesa <gidesa@interfree.it>
2023-01-27 15:12:18 +0100

    Add files via upload

---
+++ examples-Delphi/featureTest/featureTest.dpr
@@ -0,0 +1,14 @@
+program featureTest;
+
+uses
+  Vcl.Forms,
+  frmFeatureTest in 'frmFeatureTest.pas' {Form6};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm6, Form6);
+  Application.Run;
+end.
